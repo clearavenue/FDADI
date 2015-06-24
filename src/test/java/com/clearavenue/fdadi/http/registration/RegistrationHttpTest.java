8b7196e19257be601083db1f75af22a34589f047
Timothy Scott <timothy.scott@clearavenue.com>
2015-06-24 16:45:23 -0400

    Removed unused imports.

--- src/test/java/com/clearavenue/fdadi/http/registration/RegistrationHttpTest.java
+++ src/test/java/com/clearavenue/fdadi/http/registration/RegistrationHttpTest.java
@@ -1,16 +1,11 @@
 package com.clearavenue.fdadi.http.registration;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
-import javax.servlet.http.HttpSession;
-
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 import org.mongodb.morphia.Datastore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -28,41 +23,42 @@ public class RegistrationHttpTest {
 	private static final Datastore mongo = MongoDB.instance().getDatabase();
 	private static final UserProfileDAO dao = new UserProfileDAO(mongo);
 
-	private final String testUserId = "registrationtestuser";  
+	private final String testUserId = "registrationtestuser";
 	private final String testUserPwd = "newpwd";
 
 	@Before
 	public void init() {
 
-		DesiredCapabilities capabilities = DesiredCapabilities.htmlUnitWithJs();
+		final DesiredCapabilities capabilities = DesiredCapabilities.htmlUnitWithJs();
 		capabilities.setBrowserName("firefox");
 		driver = new HtmlUnitDriver(capabilities);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-		driver.get("https://agile.clearavenue.com/FDADI");  
-		
+		driver.get("https://agile.clearavenue.com/FDADI");
+
 		mongo.findAndDelete(mongo.createQuery(UserProfile.class).field("userId").equal(testUserId));
 	}
-	
-//	@Test
+
+	// @Test
 	public void validRegistrationTest() {
 		WebElement element = driver.findElement(By.name("username"));
-		element.sendKeys(testUserId);  
+		element.sendKeys(testUserId);
 
 		element = driver.findElement(By.name("pwd"));
-		element.sendKeys(testUserPwd);   
+		element.sendKeys(testUserPwd);
 
 		element = driver.findElement(By.id("registerButton"));
-		element.click();   
+		element.click();
 
-		String expected = "FDADI - registrationtestuser";
-		String actual = driver.getTitle();
+		final String expected = "FDADI - registrationtestuser";
+		final String actual = driver.getTitle();
 		System.out.println(actual);
 		assertEquals(expected, actual);
 	}
-//	@Test
+
+	// @Test
 	public void invalidRegisterTest() {
-		UserProfile newuser = new UserProfile(testUserId, testUserPwd);
+		final UserProfile newuser = new UserProfile(testUserId, testUserPwd);
 		dao.save(newuser);
 
 		WebElement element = driver.findElement(By.name("username"));
@@ -72,9 +68,9 @@ public class RegistrationHttpTest {
 		element.sendKeys(testUserPwd);
 
 		element = driver.findElement(By.id("registerButton"));
-		element.click();   
+		element.click();
 
-		String url = driver.getCurrentUrl();
+		final String url = driver.getCurrentUrl();
 		System.out.println(url);
 		assertTrue(url.contains("loginError"));
 
