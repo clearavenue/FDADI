ff553a6a4b86df7186c421ab35ef700f6b0ac417
Timothy Scott <timothy.scott@clearavenue.com>
2015-06-26 13:25:06 -0400

    Added test for FDADIController recalls method

--- src/test/java/com/clearavenue/fdadi/FDADIControllerTest.java
+++ src/test/java/com/clearavenue/fdadi/FDADIControllerTest.java
@@ -3,9 +3,12 @@ package com.clearavenue.fdadi;
 import static org.junit.Assert.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -23,9 +26,12 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import com.clearavenue.data.MongoDB;
 import com.clearavenue.data.objects.UserProfile;
+import com.clearavenue.fdadi.api.RecallEvent;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring-dispatcher-servlet.xml")
@@ -49,7 +55,11 @@ public class FDADIControllerTest {
 		MockitoAnnotations.initMocks(this);
 		final FDADIController instance = new FDADIController();
 
-		mockMvc = MockMvcBuilders.standaloneSetup(instance).build();
+		final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/jsp/");
+		resolver.setSuffix(".jsp");
+		mockMvc = MockMvcBuilders.standaloneSetup(instance).setViewResolvers(resolver).build();
 
 		mongo.findAndDelete(mongo.createQuery(UserProfile.class).field("userId").equal(testUserId));
 	}
@@ -155,6 +165,22 @@ public class FDADIControllerTest {
 			final ResultActions results = mockMvc.perform(request);
 			results.andExpect(MockMvcResultMatchers.redirectedUrl("/"));
 			results.andExpect(request().sessionAttribute("username", Matchers.equalTo(testUserId)));
+		} catch (final Exception e) {
+			fail("Exception: " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void recallTest() {
+		final MockHttpServletRequestBuilder request = post("/recalls").session(session).param("medlist", "Abilify---,---Ibuprofen---,---");
+		try {
+
+			final ResultActions results = mockMvc.perform(request);
+			results.andExpect(model().attribute("recallList", Matchers.notNullValue()));
+			results.andExpect(model().attribute("recallList", Matchers.instanceOf(List.class)));
+			results.andExpect(model().attribute("recallList", Matchers.hasItem(Matchers.instanceOf(List.class))));
+			results.andExpect(model().attribute("recallList", Matchers.hasItem(Matchers.hasItem(Matchers.instanceOf(RecallEvent.class)))));
+
 		} catch (final Exception e) {
 			fail("Exception: " + e.getMessage());
 		}
