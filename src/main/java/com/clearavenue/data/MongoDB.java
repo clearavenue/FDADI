8c227946dd1ebb937b96986b913f0b4a307bef25
bill.hunt <bill.hunt@clearavenue.com>
2015-06-26 17:12:27 -0400

    added properties file

--- src/main/java/com/clearavenue/data/MongoDB.java
+++ src/main/java/com/clearavenue/data/MongoDB.java
@@ -1,5 +1,8 @@
 package com.clearavenue.data;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.slf4j.Logger;
@@ -20,13 +23,17 @@ public class MongoDB {
 	private Datastore datastore;
 
 	public static final String DB_NAME = "fdadi";
-	private static final String dbIP = "AgileBPA-0.clearavenue.0553.mongodbdns.com";
-	private static final int dbPort = 27000;
 
 	protected MongoClient mongoClient;
 
 	private MongoDB() {
 		try {
+			Properties properties = new Properties();
+			properties.load(getClass().getClassLoader().getResourceAsStream("myMedications.properties"));
+			String dbIP = properties.getProperty("mongoServerHost");
+			int dbPort = Integer.parseInt(properties.getProperty("mongoServerPort"));
+
+			logger.info("Connecting to [{}:{}]", dbIP, dbPort);
 			mongoClient = new MongoClient(dbIP, dbPort);
 			mongoClient.setWriteConcern(WriteConcern.ACKNOWLEDGED);
 
@@ -34,7 +41,7 @@ public class MongoDB {
 			datastore.ensureIndexes();
 
 			logger.info("Connection to database [{}] initialized", DB_NAME);
-		} catch (final Exception e) {
+		} catch (final IOException e) {
 			logger.error("Failed to init MongoDB: {}.", e.getMessage());
 		}
 	}
